abab4ad3eab56d8c76835bc16b9a46b987314006
Brunobmp <38910339+Brunobmp@users.noreply.github.com>
2024-05-08 14:27:26 -0300

    Update caption-vton-dataset.py

--- data-processing/caption-vton-dataset.py
+++ data-processing/caption-vton-dataset.py
@@ -3,29 +3,58 @@
 Captioning model:
     https://huggingface.co/docs/transformers/main/en/model_doc/idefics2
 """
-from transformers import Idefics2Processor, Idefics2ForConditionalGeneration
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from accelerate import Accelerator
+import matplotlib.pyplot as plt
 from pathlib import Path
 from PIL import Image
+from tqdm import tqdm
 import pandas as pd 
 import numpy as np 
 import requests
+import torch
 import os 
 
 root = [x for x in 
         [Path(r"E:\backups\toptal\pixelcut\virtual-try-on\viton-partial"),
          Path("/mnt/vdb/datasets/viton_combined_annotated/viton_combined_annotated")]
         if os.path.exists(x)][0]
-BATCH_SZ=2
+BATCH_SZ=400
 
-processor = Idefics2Processor.from_pretrained("HuggingFaceM4/idefics2-8b")
-model = Idefics2ForConditionalGeneration.from_pretrained("HuggingFaceM4/idefics2-8b")
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to("cuda")
 
-messages = [{
-    "role": "user",
-    "content": [
-        {"type": "text", "text": "What’s the difference between these two images?"},
-        {"type": "image"},
-        {"type": "image"},
-    ],
-}]
+accelerator = Accelerator()
 
+allFiles = [root/x for x in os.listdir(root)]
+print("Captioning", len(allFiles))
+
+batch = []
+
+for f in tqdm(allFiles, desc="Captioning..."):
+    if os.path.isdir(f):
+        base = f/"garment"
+        fn = os.listdir(base)
+        image = Image.open(base/fn[0])
+        batch.append((image, f))
+        if len(batch) >= BATCH_SZ:
+            inputs = processor([b[0] for b in batch], return_tensors="pt").to("cuda")
+            with torch.no_grad():
+                out = model.generate(**inputs)
+            output_text = [processor.decode(o, skip_special_tokens=True) 
+                           for o in out]
+            for fi, txt in zip(batch, output_text):
+                folder = fi[1]
+                os.makedirs(folder/"caption", exist_ok=True)
+                with open(folder/"caption"/"caption.txt", "w") as file:
+                    file.write(txt)
+                
+            plt.figure(figsize=(7,7))
+            plt.title(txt)
+            plt.imshow(image)
+            plt.axis("off")
+            plt.tight_layout()
+            plt.show()
+                
+            batch = []
+            
