ebb28c75c21e795aad0992073fa80df3e308b3a2
Brunobmp <38910339+Brunobmp@users.noreply.github.com>
2024-05-13 09:02:04 -0300

    updated tests

--- tests/test-forward-pass-gradients.py
+++ tests/test-forward-pass-gradients.py
@@ -123,107 +123,79 @@ dm = SDCNVTONDataModule(
 dm.setup()
 train_dl = dm.train_dataloader()
 
-for b in train_dl:
-    sample = b
-    break
-"""
-sample.keys()
-
-Out[3]: dict_keys(['clip_image_encoder_garment_image', 'identity_image', 'identity_mask',
-                   'cloth_mask', 'caption_cloth', 'caption', 'garment_image', 'person_image',
-                   'segmentation_image', 'densepose_image'])
-"""
-
-def visualize(sample, key):
-    xi = sample[key][0]
-    xi = (xi - xi.min())/(xi.max() - xi.min())
-    plt.imshow(xi.cpu().numpy().transpose(1,2,0))
-    plt.axis("off")
-
-offset=0
-plt.figure(figsize=(16,10))
-for i, k in enumerate(sample.keys()):
-    if type(sample[k][0]) is str:
-        offset+=1
-        continue
-    plt.subplot(2,4,i+1-offset)
-    plt.title(k)
-    visualize(sample, key=k)
-plt.tight_layout()
-plt.show()
-
-with torch.cuda.amp.autocast():
-    with torch.no_grad():
-        prompt = sample["caption"]
-
-        num_prompts = sample['garment_image'].shape[0]
-        negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
-
-        if not isinstance(prompt, List):
-            prompt = [prompt] * num_prompts
-        if not isinstance(negative_prompt, List):
-            negative_prompt = [negative_prompt] * num_prompts
-
-        image_embeds = sample['clip_image_encoder_garment_image']
-
-        with torch.inference_mode():
-            (
-                prompt_embeds,
-                negative_prompt_embeds,
-                pooled_prompt_embeds,
-                negative_pooled_prompt_embeds,
-            ) = pipe.encode_prompt(
-                prompt,
-                num_images_per_prompt=1,
-                do_classifier_free_guidance=True,
-                negative_prompt=negative_prompt,
-            )
-        
-        
+for sample in train_dl:
+    with torch.cuda.amp.autocast(device_type="cuda"):
+        with torch.no_grad():
             prompt = sample["caption"]
+    
+            num_prompts = sample['garment_image'].shape[0]
             negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
-
+    
             if not isinstance(prompt, List):
                 prompt = [prompt] * num_prompts
             if not isinstance(negative_prompt, List):
                 negative_prompt = [negative_prompt] * num_prompts
-
-
+    
+            image_embeds = sample['clip_image_encoder_garment_image']
+    
             with torch.inference_mode():
                 (
-                    prompt_embeds_c,
-                    _,
-                    _,
-                    _,
+                    prompt_embeds,
+                    negative_prompt_embeds,
+                    pooled_prompt_embeds,
+                    negative_pooled_prompt_embeds,
                 ) = pipe.encode_prompt(
                     prompt,
                     num_images_per_prompt=1,
-                    do_classifier_free_guidance=False,
+                    do_classifier_free_guidance=True,
                     negative_prompt=negative_prompt,
                 )
             
-            generator = torch.Generator(pipe.device).manual_seed(42)
-            images = pipe(
-                prompt_embeds=prompt_embeds.to(accelerator.device),
-                negative_prompt_embeds=negative_prompt_embeds.to(accelerator.device),
-                pooled_prompt_embeds=pooled_prompt_embeds.to(accelerator.device),
-                negative_pooled_prompt_embeds=negative_pooled_prompt_embeds.to(accelerator.device),
-                num_inference_steps=40,
-                generator=generator,
-                strength = 1.0,
-                pose_img = sample['densepose_image'].to(accelerator.device),
-                text_embeds_cloth=prompt_embeds_c.to(accelerator.device),
-                cloth = sample["garment_image"].to(accelerator.device),
-                mask_image=sample['cloth_mask'].to(accelerator.device),
-                image=(sample['person_image'].to(accelerator.device)+1.0)/2.0, 
-                height=IMAGE_SIZE[1],
-                width=IMAGE_SIZE[0],
-                guidance_scale=2.0,
-                ip_adapter_image = image_embeds,
-            )[0]
-
-
-        for i in range(len(images)):
-            x_sample = pil_to_tensor(images[i])
-            torchvision.utils.save_image(x_sample,os.path.join("results",sample['caption'][i] + ".png"))
+            
+                prompt = sample["caption"]
+                negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
+    
+                if not isinstance(prompt, List):
+                    prompt = [prompt] * num_prompts
+                if not isinstance(negative_prompt, List):
+                    negative_prompt = [negative_prompt] * num_prompts
+    
+    
+                with torch.inference_mode():
+                    (
+                        prompt_embeds_c,
+                        _,
+                        _,
+                        _,
+                    ) = pipe.encode_prompt(
+                        prompt,
+                        num_images_per_prompt=1,
+                        do_classifier_free_guidance=False,
+                        negative_prompt=negative_prompt,
+                    )
+                
+                generator = torch.Generator(pipe.device).manual_seed(42)
+                images = pipe(
+                    prompt_embeds=prompt_embeds.to(accelerator.device),
+                    negative_prompt_embeds=negative_prompt_embeds.to(accelerator.device),
+                    pooled_prompt_embeds=pooled_prompt_embeds.to(accelerator.device),
+                    negative_pooled_prompt_embeds=negative_pooled_prompt_embeds.to(accelerator.device),
+                    num_inference_steps=40,
+                    generator=generator,
+                    strength = 1.0,
+                    pose_img = sample['densepose_image'].to(accelerator.device),
+                    text_embeds_cloth=prompt_embeds_c.to(accelerator.device),
+                    cloth = sample["garment_image"].to(accelerator.device),
+                    mask_image=sample['cloth_mask'].to(accelerator.device),
+                    image=(sample['person_image'].to(accelerator.device)+1.0)/2.0, 
+                    height=IMAGE_SIZE[1],
+                    width=IMAGE_SIZE[0],
+                    guidance_scale=2.0,
+                    ip_adapter_image = image_embeds,
+                )[0]
+    
+    
+            for i in range(len(images)):
+                x_sample = pil_to_tensor(images[i])
+                torchvision.utils.save_image(x_sample,os.path.join("results",sample['caption'][i] + ".png"))
         
